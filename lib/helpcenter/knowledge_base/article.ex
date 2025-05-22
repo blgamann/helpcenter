0d5f171d59176fffe88ea84f5884291cd1ab35eb
0xdagarn <sgnoo@naver.com>
2025-05-22 22:28:43 +0900

    chore: ash migration

---
+++ lib/helpcenter/knowledge_base/article.ex
@@ -0,0 +1,83 @@
+defmodule Helpcenter.KnowledgeBase.Article do
+  use Ash.Resource,
+    domain: Helpcenter.KnowledgeBase,
+    data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "articles"
+    repo Helpcenter.Repo
+  end
+
+  attributes do
+    uuid_primary_key :id
+    attribute :title, :string, allow_nil?: false
+    attribute :slug, :string
+    attribute :content, :string
+    attribute :views_count, :integer, default: 0
+    attribute :published, :boolean, default: false
+    timestamps()
+  end
+
+  relationships do
+    belongs_to :category, Helpcenter.KnowledgeBase.Category do
+      source_attribute :category_id
+      allow_nil? false
+    end
+
+    has_many :comments, Helpcenter.KnowledgeBase.Comment do
+      destination_attribute :article_id
+    end
+
+    many_to_many :tags, Helpcenter.KnowledgeBase.Tag do
+      through Helpcenter.KnowledgeBase.ArticleTag
+      source_attribute_on_join_resource :article_id
+      destination_attribute_on_join_resource :tag_id
+    end
+
+    has_many :article_feedbacks, Helpcenter.KnowledgeBase.ArticleFeedback do
+      destination_attribute :article_id
+    end
+  end
+
+  actions do
+    default_accept [
+      :title,
+      :slug,
+      :content,
+      :views_count,
+      :published,
+      :category_id # added for category relationship
+  ]
+    defaults [:create, :read, :update, :destroy]
+
+    create :create_with_category do
+      description "Create an article and a category at the same time"
+      argument :category_attrs, :map, allow_nil?: false
+      change manage_relationship(:category_attrs, :category, type: :create)
+    end
+
+    update :add_comment do
+      description "Add a comment to an article"
+      require_atomic? false
+      argument :comment, :map, allow_nil?: false
+      change manage_relationship(:comment, :comments, type: :create)
+    end
+
+    update :add_feedback do
+      description "Add a feedback to an article"
+      require_atomic? false
+      argument :feedback, :map, allow_nil?: false
+      change manage_relationship(:feedback, :article_feedbacks, type: :create)
+    end
+
+    create :create_with_tags do
+      description "Create an article with tags"
+      argument :tags, {:array, :map}, allow_nil?: false
+      change manage_relationship(:tags, :tags,
+        on_no_match: :create,
+        on_match: :ignore,
+        on_missing: :create
+      )
+    end
+  end
+end
---
+++ lib/helpcenter/knowledge_base/category.ex
@@ -0,0 +1,46 @@
+defmodule Helpcenter.KnowledgeBase.Category do
+  use Ash.Resource,
+    domain: Helpcenter.KnowledgeBase,
+    data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "categories"
+    repo Helpcenter.Repo
+    references do
+      reference :articles, on_delete: :delete
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+    attribute :name, :string, allow_nil?: false
+    attribute :slug, :string
+    attribute :description, :string, allow_nil?: true
+    timestamps()
+  end
+
+  relationships do
+    has_many :articles, Helpcenter.KnowledgeBase.Article do
+      description "Relationship with the articles."
+      destination_attribute :category_id
+    end
+  end
+
+  actions do
+    default_accept [:name, :slug, :description]
+    defaults [:create, :read, :update, :destroy]
+
+    update :create_article do
+      description "Create an an article under a specified category"
+      require_atomic? false
+      argument :article_attrs, :map, allow_nil?: false
+      change manage_relationship(:article_attrs, :articles, type: :create)
+    end
+
+    create :create_with_article do
+      description "Create a category and an article at the same time"
+      argument :article_attrs, :map, allow_nil?: false
+      change manage_relationship(:article_attrs, :articles, type: :create)
+    end
+  end
+end
