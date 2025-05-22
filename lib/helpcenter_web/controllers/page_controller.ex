a804922796f9fd214daba9fb61ecb946eddbf21e
0xdagarn <sgnoo@naver.com>
2025-05-22 22:42:44 +0900

    feat: add html for view

--- lib/helpcenter_web/controllers/page_controller.ex
+++ lib/helpcenter_web/controllers/page_controller.ex
@@ -1,9 +1,13 @@
 defmodule HelpcenterWeb.PageController do
+  alias Helpcenter.KnowledgeBase.Category
   use HelpcenterWeb, :controller
 
   def home(conn, _params) do
+    # Retrieve categories with the articles
+    categories = Ash.read!(Category, load: :articles)
+
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home, layout: false, categories: categories)
   end
-end
+ end
